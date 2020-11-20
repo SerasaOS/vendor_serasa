92073f16e62afd6d79552c7210391a57c80d96ff
HeroBuxx <me@herobuxx.xyz>
2020-11-20 12:08:22 +0530

    vendor: serasa Begins.

---
+++ build/tasks/carthage.mk
@@ -0,0 +1,30 @@
+# Copyright (C) 2020 The ConquerOS Project
+# Copyright (C) 2021 a xdroid Prjkt
+# Copyright (C) 2023 Serasa Prjkt
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+serasa_TARGET_PACKAGE := $(PRODUCT_OUT)/$(serasa_BUILD_VERSION).zip
+MD5 := prebuilts/build-tools/path/$(HOST_OS)-x86/md5sum
+
+.PHONY: carthage
+carthage: $(INTERNAL_OTA_PACKAGE_TARGET)
+	$(hide) mv $(INTERNAL_OTA_PACKAGE_TARGET) $(SERASA_TARGET_PACKAGE)
+	$(hide) $(MD5) $(SERASA_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(SERASA_TARGET_PACKAGE).md5sum
+	@echo "Done"
+	@echo -e "\t ===============================-Package complete-============================================================="
+	@echo -e "\t Zip: $(SERASA_TARGET_PACKAGE)"
+	@echo -e "\t MD5: `cat $(SERASA_TARGET_PACKAGE).md5sum | awk '{print $$1}' `"
+	@echo -e "\t Size: `du -sh $(SERASA_TARGET_PACKAGE) | awk '{print $$1}' `"
+	@echo -e "\t SerasaCLO - Keep Minimalist"
+	@echo -e "\t =============================================================================================================="
---
+++ config/version.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2020 The ConquerOS Project
+#				2021 a SERASA Prjkt
+#				2023 Serasa Prjkt
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+SERASA_HOST_TIME := $(shell date +"%Y%m%d-%H%M")
+
+ifndef SERASA_BUILD_TYPE
+   SERASA_BUILD_TYPE := UNOFFICIAL
+endif
+
+SERASA_VERSION := X11
+SERASA_BUILD_VERSION := SERASA$(SERASA_VERSION)-$(SERASA_BUILD)-$(SERASA_HOST_TIME)-$(SERASA_BUILD_TYPE)
+SERASA_BUILD_NUMBER := xd.$(SERASA_VERSION).$(SERASA_HOST_TIME)
+CAF_REVISION_TAG := LA.UM.9.6.2.r1-02100-89xx.0
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+   ro.serasa.version=$(SERASA_VERSION) \
+   ro.serasa.build.version=$(SERASA_BUILD_VERSION) \
+   ro.serasa.build.number=$(SERASA_BUILD_NUMBER) \
+   ro.serasa.build.type=$(SERASA_BUILD_TYPE) \
+   ro.caf.revision.tag=$(CAF_REVISION_TAG)
