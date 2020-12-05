d8d4c82ac94635723acdac6cf6c8b449af67cb78
Jake Weinstein <jake@aospa.co>
2020-12-05 22:32:03 +0530

    vendor: import sdclang configs
    Signed-off-by: Kneba <abenkenary3@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -107,6 +107,19 @@ PRODUCT_COPY_FILES += \
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/serasa/overlay/dictionaries
 
+ifneq ($(HOST_OS),linux)
+ifneq ($(sdclang_already_warned),true)
+$(warning **********************************************)
+$(warning * SDCLANG is not supported on non-linux hosts.)
+$(warning **********************************************)
+sdclang_already_warned := true
+endif
+else
+
+# include definitions for SDCLANG
+include vendor/serasa/sdclang/sdclang.mk
+endif
+
 # Include AOSP audio files
 include vendor/serasa/config/aosp_audio.mk
 
