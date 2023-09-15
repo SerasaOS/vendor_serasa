ConquerOS/vendor_conquer

config: build common required packages
Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

config/common.mk
@@ -114,5 +114,8 @@ include vendor/conquer/config/conquer_audio.mk
 # Include ConquerUI
 include vendor/conquerui/config.mk
 
+# Include Common packages
+include vendor/conquer/config/packages.mk
+
 # Include Version
 include vendor/conquer/config/version.mk

SerasaOS/vendor_serasa

Signed-off-by: Kneba <abenkenary3@gmail.com>

config/common.mk
@@ -116,5 +116,8 @@ include vendor/serasa/config/serasa_audio.mk
 # Include serasaUI
 include vendor/serasaui/config.mk
 
+# Include serasa packages
+include vendor/serasa/config/packages.mk
+
 # Include Version
 include vendor/serasa/config/version.mk