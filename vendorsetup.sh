ConquerOS/vendor_conquer

vendorsetup: move sdclang variable

vendorsetup.sh
@@ -1,1 +1,4 @@
 export CAF_REVISION_TAG=$(grep "revision" ".repo/manifests/snippets/system.xml" | awk -F '/' '{print $3}' | awk -F '"' '{print $1}')
+
+# SDClang Environment Variables
+export SDCLANG_CONFIG=$(pwd)/vendor/conquer/sdclang/sdclang.json

SerasaOS/vendor_serasa

vendorsetup.sh
@@ -1,1 +1,4 @@
 export CAF_REVISION_TAG=$(grep "revision" ".repo/manifests/snippets/system.xml" | awk -F '/' '{print $3}' | awk -F '"' '{print $1}')
+
+# SDClang Environment Variables
+export SDCLANG_CONFIG=$(pwd)/vendor/serasa/sdclang/sdclang.json